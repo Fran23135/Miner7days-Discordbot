889bd7b4bae43746fdac31b6d319f76486f760de
FranDev <134436020+Fran23135@users.noreply.github.com>
2026-06-18 15:33:33 -0600

    Update pin.py

--- pin.py
+++ pin.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=".env")
 
-TOKEN = os.getenv("TOKEN")  # Cambia a TOKEN 
-TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
-TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
-IMGBB_API = os.getenv("IMGBB_API")
-NETLIFY_TOKEN = os.getenv("NETLIFY_TOKEN")
-NETLIFY_SITE_ID = os.getenv("NETLIFY_SITE_ID")
-NETLIFY_BASE_URL = os.getenv("NETLIFY_BASE_URL")
-NGROK_TOKEN  = os.getenv("_NGROK_TOKEN")
+TOKEN = os.environ.get("TOKEN")  # Cambia a TOKEN 
+TWITCH_CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.environ.get("TWITCH_CLIENT_SECRET")
+IMGBB_API = os.environ.get("IMGBB_API")
+NETLIFY_TOKEN = os.environ.get("NETLIFY_TOKEN")
+NETLIFY_SITE_ID = os.environ.get("NETLIFY_SITE_ID")
+NETLIFY_BASE_URL = os.environ.get("NETLIFY_BASE_URL")
+NGROK_TOKEN  = os.environ.get("_NGROK_TOKEN")
