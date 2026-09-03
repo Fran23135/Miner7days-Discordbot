833ceb23ee36a15d32fe382dbce489525dc8cff1
FranDev <134436020+Fran23135@users.noreply.github.com>
2026-09-03 12:08:09 -0600

    Update pin.py

--- pin.py
+++ pin.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=".env")
 
-TOKEN = os.getenv("TOKEN_TEST")  # Cambia a TOKEN 
+TOKEN = os.getenv("TOKEN")  # Cambia a TOKEN 
 TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
 TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 IMGBB_API = os.getenv("IMGBB_API")
